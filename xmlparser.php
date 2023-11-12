fb1758d1c996b7a8b7519181033801f4a4c4c6de
Quimoniz <quimoniz@resonanzkaska.de>
2023-11-12 19:18:55 +0100

    Improved the Simple XML Parser.
    My self written simple xml parser now also recognizes comments,
    thus it now can also handle comments in XML which contain XML tags.
    
    Comment styles supported: '<!-- ... -->' and '<![CDATA[ ... ]]>'

--- xmlparser.php
+++ xmlparser.php
@@ -75,6 +75,7 @@ class XmlNode {
 }
 function parseXml($xml_code)
 {
+	$TREAT_COMMENT_AS_TEXTCONTENT = TRUE;
 	$within_tag = FALSE;
 	$scanning_tag_name = FALSE;
 	$within_attribute = FALSE;
@@ -87,78 +88,95 @@ function parseXml($xml_code)
 	$xml_len = strlen($xml_code);
 	$root_element = NULL;
 	$do_parse_attributes = TRUE;
+	$special_out_of_syntax = FALSE;
 	for($i = 0; $i < $xml_len; $i++)
 	{
 		$c = $xml_code[$i];
-		if(!$within_tag)
+		// if we are not in a comment
+		if(!$special_out_of_syntax)
 		{
-			if('<' == $c)
+			if(!$within_tag)
 			{
-				$within_tag = TRUE;
-				$scanning_tag_name = TRUE;
-				$cur_tag_name = "";
-			} else
-			{
-				if($cur_element)
+				if('<' == $c)
 				{
-					$cur_element->text .= $c;
-				}
-			}
-		// TRUE == $within_tag
-		} else
-		{
-			if($scanning_tag_name)
-			{
-				if(is_space($c) || '>' == $c)
-				{
-					if(0 < strlen($cur_tag_name) && ('?' == $cur_tag_name[0] || 0 === strpos($cur_tag_name, '!--')))
-					{
-						// if tagName starts with a question or exclamation mark
-						// do nothing
-						$do_parse_attributes = FALSE;
-					} elseif(0 < strlen($cur_tag_name) && '/' == $cur_tag_name[0])
-					{
-						// maybe TODO: check if $cur_tag_name (without slash) fits current node's tagName
-						if($cur_element->parentNode)
-						{
-							$cur_element = $cur_element->parentNode;
-						} else
-						{
-							return $root_element;
-						}
-					} else
-					{
-						$new_node = new XmlNode($cur_tag_name, $cur_element);
-						$cur_element->children[] = $new_node;
-						if(!$new_node->parentNode)
-						{
-							$root_element = $new_node;
-						}
-						$cur_element = $new_node;
-						$do_parse_attributes = TRUE;
-					}
-					if('>' == $c)
-					{
-						$within_tag = FALSE;
-					} else
-					{
-						$scanning_attribute_name = TRUE;
-					}
-					$scanning_tag_name = FALSE;
+					$within_tag = TRUE;
+					$scanning_tag_name = TRUE;
+					$cur_tag_name = "";
 				} else
 				{
-					$cur_tag_name .= $c;
+					if($cur_element)
+					{
+						$cur_element->text .= $c;
+					}
 				}
-			// FALSE == $scanning_tag_name
+			// TRUE == $within_tag
 			} else
 			{
-				if(FALSE === $within_attribute)
+				if($scanning_tag_name)
 				{
 					if(is_space($c) || '>' == $c)
 					{
-						if(0 < strlen($cur_attribute_name) && $cur_element && $do_parse_attributes)
+						// if tagName starts with a question or exclamation mark
+						if(0 < strlen($cur_tag_name) && ('?' == $cur_tag_name[0] || 0 === strpos($cur_tag_name, '!')))
 						{
-							$cur_element->attributes[$cur_attribute_name] = $cur_attribute_value;
+	
+							//    Standard Comments <!-- -->
+							if(0 === strpos($cur_tag_name, '!--'))
+							{
+								$scanning_tag_name = FALSE;
+								$do_parse_attributes = FALSE;
+								$within_tag = FALSE;
+								$special_out_of_syntax = TRUE;
+								$special_ending_chars_str = '-->';
+								$special_ending_chars_index = 0;
+								if($TREAT_COMMENT_AS_TEXTCONTENT
+								 && $cur_element)
+								{
+									$cur_element->text .= '<' . $cur_tag_name . $c;
+								}
+							//    CDATA stuff like <![CDATA[.....]]>
+							} elseif(0 === strpos($cur_tag_name, '![CDATA['))
+							{
+								$scanning_tag_name = FALSE;
+								$do_parse_attributes = FALSE;
+								$within_tag = FALSE;
+								$special_out_of_syntax = TRUE;
+								$special_ending_chars_str = ']]>';
+								$special_ending_chars_index = 0;
+								if($TREAT_COMMENT_AS_TEXTCONTENT
+								 && $cur_element)
+								{
+									$cur_element->text .= '<' . $cur_tag_name . $c;
+								}
+							} else {
+								// do nothing,
+								// i.e. for "<?" xml tags...
+								$do_parse_attributes = FALSE;
+							}
+						// ending tag like </tagName>
+						} elseif(0 < strlen($cur_tag_name) && '/' == $cur_tag_name[0])
+						{
+							// maybe TODO: check if $cur_tag_name (without slash) fits current node's tagName
+							if($cur_element->parentNode)
+							{
+								$cur_element = $cur_element->parentNode;
+							} else
+							{
+								return $root_element;
+							}
+						// beginning tag like <tagName>
+						} else
+						{
+							$new_node = new XmlNode($cur_tag_name, $cur_element);
+							if(NULL !== $cur_element)
+							{
+								$cur_element->children[] = $new_node;
+							} else if(!$new_node->parentNode)
+							{
+								$root_element = $new_node;
+							}
+							$cur_element = $new_node;
+							$do_parse_attributes = TRUE;
 						}
 						if('>' == $c)
 						{
@@ -167,64 +185,109 @@ function parseXml($xml_code)
 						{
 							$scanning_attribute_name = TRUE;
 						}
-						$cur_attribute_name = "";
-						$cur_attribute_value = "";
-					// no space and $c != '>'
+						$scanning_tag_name = FALSE;
 					} else
 					{
-						if($scanning_attribute_name)
-						{
-							if('=' == $c)
-							{
-								$scanning_attribute_name = FALSE;
-							} else
-							{
-								$cur_attribute_name .= $c;
-							}
-						} else
-						{
-							if('"' == $c)
-							{
-								$within_attribute = TRUE;
-							} else
-							{
-								$cur_attribute_value .= $c;
-							}
-						}
+						$cur_tag_name .= $c;
 					}
-				// TRUE == $within_attribute
+				// FALSE == $scanning_tag_name
 				} else
 				{
-					if($within_escape)
+					if(FALSE === $within_attribute)
 					{
-						switch($c)
+						if(is_space($c) || '>' == $c)
 						{
-							case "n":
-								$cur_attribute_value .= "\n";
-								break;
-							case "r":
-								$cur_attribute_value .= "\r";
-								break;
-							case "t":
-								$cur_attribute_value .= "\t";
-								break;
-							default:
-								$cur_attribute_value .= $c;
-							
+							if(0 < strlen($cur_attribute_name) && $cur_element && $do_parse_attributes)
+							{
+								$cur_element->attributes[$cur_attribute_name] = $cur_attribute_value;
+							}
+							if('>' == $c)
+							{
+								$within_tag = FALSE;
+							} else
+							{
+								$scanning_attribute_name = TRUE;
+							}
+							$cur_attribute_name = "";
+							$cur_attribute_value = "";
+						// no space and $c != '>'
+						} else
+						{
+							if($scanning_attribute_name)
+							{
+								if('=' == $c)
+								{
+									$scanning_attribute_name = FALSE;
+								} else
+								{
+									$cur_attribute_name .= $c;
+								}
+							} else
+							{
+								if('"' == $c)
+								{
+									$within_attribute = TRUE;
+								} else
+								{
+									$cur_attribute_value .= $c;
+								}
+							}
 						}
-						$within_escape = false;
-					}
-					if("\\" == $c)
-					{
-						$within_escape = TRUE;
-					} elseif("\"" == $c)
-					{
-						$within_attribute = FALSE;
+					// TRUE == $within_attribute
 					} else
 					{
-						$cur_attribute_value .= $c;
+						if($within_escape)
+						{
+							switch($c)
+							{
+								case "n":
+									$cur_attribute_value .= "\n";
+									break;
+								case "r":
+									$cur_attribute_value .= "\r";
+									break;
+								case "t":
+									$cur_attribute_value .= "\t";
+									break;
+								default:
+									$cur_attribute_value .= $c;
+								
+							}
+							$within_escape = false;
+						}
+						if("\\" == $c)
+						{
+							$within_escape = TRUE;
+						} elseif("\"" == $c)
+						{
+							$within_attribute = FALSE;
+						} else
+						{
+							$cur_attribute_value .= $c;
+						}
 					}
 				}
+	
+			// TRUE == $special_out_of_syntax
+			// if we are in a comment like <!--  ...  -->
+			}
+		} else
+		{
+			if($TREAT_COMMENT_AS_TEXTCONTENT
+			 && $cur_element)
+			{
+				$cur_element->text .= $c;
+			}
+			if(ord($c) === ord($special_ending_chars_str[$special_ending_chars_index]))
+			{
+				$special_ending_chars_index += 1;
+				if(strlen($special_ending_chars_str) <= $special_ending_chars_index)
+				{
+					$special_out_of_syntax = FALSE;
+				}
+			} else
+			{
+				$special_ending_chars_index = 0;
 			}
 		}
 	}
